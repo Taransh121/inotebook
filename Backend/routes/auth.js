e94e5f4c0caca528d292991997916b7b480d308b
TARANSH CHELLANI <taranshchellani121@gmail.com>
2022-11-15 15:41:06 +0530

    video 46 completed

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,17 +1,33 @@
-const express=require("express");
-const User=require("../models/User")
-const router=express.Router()    //jo hum app.get ya app.post likhte hai ab vo hum router.get and router.post krke likhenge.
+const express = require("express");
+const User = require("../models/User")
+const router = express.Router()    //jo hum app.get ya app.post likhte hai ab vo hum router.get and router.post krke likhenge.
+const { body, validationResult } = require('express-validator'); //Data validation
 
 //Create a user using POST "api/auth/" , Doesnt require auth.
-router.post("/",(req,res)=>{
-    //printing user's body-
-    console.log(req.body);
-    //Creating a new user-
-    const newuser=User(req.body);
-    //saving the user to the database-
-    newuser.save();
-    //sending user's body as response-
-    res.send(req.body)
+router.post("/", [
+    //Is array ke andar hum apne saare validations likhenge.
+    // email must be an email
+    body('email',"Enter a valid email").isEmail(),
+    // name must be at least 3 chars long
+    body('name',"Enter a valid name").isLength({ min: 3 }),
+    // password must be at least 5 chars long
+    body('password',"Password must be atleast 5 characters").isLength({ min: 5 }),
+
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {  //agar errors empty nhi hai toh hum ek 400 bad request bhejenge aur jo errors hai unko bhejenge 
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //Creating a user
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user)) //Sending user's data as response
+      .catch((error)=>{   //Catching an error if it occurs due to already present email address.
+        console.log(error);
+        res.json({error:"Please enter a unique email."})
+    });
 })
 
-module.exports=router;
+module.exports = router;
