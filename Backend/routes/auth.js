a69058b0d0a864292906dd95ba89395681cd80c8
TARANSH CHELLANI <taranshchellani121@gmail.com>
2022-11-16 15:27:34 +0530

    video 49 completed

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User")
 const router = express.Router()    //jo hum app.get ya app.post likhte hai ab vo hum router.get and router.post krke likhenge.
 const { body, validationResult } = require('express-validator'); //Data validation
+const bcrypt=require("bcryptjs");  
+const jwt = require('jsonwebtoken');
+
+const jwtSecret="70707"
 
 //Create a user using POST "api/auth/createuser" , Doesnt require auth.
 router.post("/createuser", [
@@ -24,12 +28,21 @@ router.post("/createuser", [
         if(newuser){
             return (res.status(400).json({error:"Sorry a user with this email already exists"}))  //If email alr exists it gives this response .
         }
+        const salt=await bcrypt.genSalt(10);
+        const securedPAssword=await bcrypt.hash(req.body.password,salt);
         newuser= await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: securedPAssword,
         })
-        res.json(newuser)
+        const data={
+            newuser:{
+                id:newuser.id
+            }
+        }
+        const authToken=jwt.sign(data,jwtSecret);
+        res.json({authToken});   //ab jab bhi hum ek new user create krenge toh vo hume ek authToken provide krega jisko use krke hum vaps se us authToken ko convert kr skte hai us data mai aur apne secret ki help se we can check if anyone has tampered with it.We will check it using a function "jwt.verify()"
+        // res.json(newuser)
     } catch (error) {
         console.log(error);
         res.status(500).send("Some error occurred")
