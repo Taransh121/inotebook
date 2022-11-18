3dd3a17ae2b045350778255a1225f0e42a5632e9
TARANSH CHELLANI <taranshchellani121@gmail.com>
2022-11-19 02:30:12 +0530

    video 54 completed

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,15 +1,16 @@
-const express=require("express");
+const express = require("express");
 const fetchuser = require("../middleware/fetchuser");
-const router=express.Router()    //jo hum app.get ya app.post likhte hai ab vo hum router.get and router.post krke likhenge.
-const Notes=require("../models/Notes")
+const router = express.Router()    //jo hum app.get ya app.post likhte hai ab vo hum router.get and router.post krke likhenge.
+const Notes = require("../models/Notes")
 const { body, validationResult } = require('express-validator'); //Data validation
+const { request } = require("express");
 
 
 
 //ROUTE 1 - Get all the routes using GET request /api/notes/fetchallnotes. Login Required.
-router.get("/fetchallnotes",fetchuser,async (req,res)=>{
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
     try {
-        const notes=await Notes.find({user:req.user.id}); //kyuki humne fetchuser vale middleware ko use kiya hai toh already req mai user or uski id hogi.
+        const notes = await Notes.find({ user: req.user.id }); //kyuki humne fetchuser vale middleware ko use kiya hai toh already req mai user or uski id hogi.
         res.json(notes)
     } catch (error) {
         console.log(error);
@@ -19,20 +20,20 @@ router.get("/fetchallnotes",fetchuser,async (req,res)=>{
 
 
 //ROUTE 2 - Add a new note using POST request /api/notes/addnotes. Login Required.
-router.post("/addnotes",fetchuser, [
+router.post("/addnotes", fetchuser, [
     body('title', "Enter a valid title").isLength({ min: 3 }),
     body('description', "Enter a valid description of minimum 5 characters").isLength({ min: 5 }),
-],async (req,res)=>{
+], async (req, res) => {
     try {
-        const {title,description,tag}=req.body; //Destructuring krke hum log body me se title description or tags bahar nikalre hai.
+        const { title, description, tag } = req.body; //Destructuring krke hum log body me se title description or tags bahar nikalre hai.
         const errors = validationResult(req);
         if (!errors.isEmpty()) {  //agar errors empty nhi hai toh hum ek 400 bad request bhejenge aur jo errors hai unko bhejenge 
             return res.status(400).json({ errors: errors.array() });
         }
-        const newnote=new Notes({
-            title,description,tag,user:req.user.id
+        const newnote = new Notes({
+            title, description, tag, user: req.user.id
         })
-        const savedNote=await newnote.save();
+        const savedNote = await newnote.save();
         res.json(savedNote)
     } catch (error) {
         console.log(error);
@@ -40,27 +41,52 @@ router.post("/addnotes",fetchuser, [
     }
 })
 
-module.exports=router;
+module.exports = router;
 
 //ROUTE 3 - Update an existing note PUT request /api/notes/updatenote. Login Required.Also the id of the note which we want to update is required
-router.put("/updatenote/:id",fetchuser,async (req,res)=>{
-    const {title,description,tag}=req.body;
-    //Create a new note object
-    const newNote={};
-    if(title){newNote.title=title}; //Agar title arha hai naya toh usko newNote ke title ke equal krdo.
-    if(description){newNote.description=description};
-    if(tag){newNote.tag=tag};
-
-    //find the note to be updated and update it.
-    let note=await Notes.findById(req.params.id); //Ab ye vahi note hai jiski upar endpoint me id given hai.
-    if(!note){
-        return res.status(404).send("Note found")
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+        //Create a new note object
+        const newNote = {};
+        if (title) { newNote.title = title }; //Agar title arha hai naya toh usko newNote ke title ke equal krdo.
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+    
+        //find the note to be updated and update it.
+        let note = await Notes.findById(req.params.id); //Ab ye vahi note hai jiski upar endpoint me id given hai.
+        if (!note) {
+            return res.status(404).send("Note found")
+        }
+        if (note.user.toString() != req.user.id) //Agar jiska note hai vo user aur jo user request send krra hai dono same nhi hai toh koi hack krna chahra hai.
+        {
+            return res.status(401).send("Not allowed")
+        }
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote })
+        res.json({ note })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error.")
     }
-    if(note.user.toString()!= req.user.id) //Agar jiska note hai vo user aur jo user request send krra hai dono same nhi hai toh koi hack krna chahra hai.
-    {
-        return res.status(401).send("Not allowed")
+})
+// ROUTE 4 - Delete an exisiting note using DELETE request /api/notes/deletenote. Login required.Also the id of the note which we want to update is required.
+router.delete("/deletenote/:id", fetchuser, async (req, res) => {
+    const { title, description, tag } = req.body;
+    try {
+        //find the note to be updated and delete it.
+        let note = await Notes.findById(req.params.id); //Ab ye vahi note hai jiski upar endpoint me id given hai.
+        if (!note) {
+            return res.status(404).send("Note found")
+        }
+        //Allow deleteion only if user owns this note.
+        if (note.user.toString() != req.user.id) //Agar jiska note hai vo user aur jo user request send krra hai dono same nhi hai toh koi hack krna chahra hai.
+        {
+            return res.status(401).send("Not allowed")
+        }
+        note = await Notes.findByIdAndDelete(req.params.id);
+        res.json({ "Success": "Note has been deleted" })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error.")
     }
-    note= await Notes.findByIdAndUpdate(req.params.id, {$set:newNote} )
-    res.json({note})
-
 })
